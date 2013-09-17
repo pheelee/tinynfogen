484b731dc453ec5c29fab5d47e99dba5e18f77b3
pheelee <pri@irbe.ch>
2013-09-17 07:28:25 +0200

    demo change

--- TinyNFOGen.py
+++ TinyNFOGen.py
@@ -11,6 +11,7 @@ Search URL: http://api.themoviedb.org/3/search/movie?api_key=xxx&query=after+the
 Detail URL: http://api.themoviedb.org/3/movie/10589?api_key=xxx&language=de
 
 
+
 '''
 import os,sys,signal
 
